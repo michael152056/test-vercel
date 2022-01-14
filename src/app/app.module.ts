0347f9aa557f3ce4eb8a0c11f1793a4a7cbf3eb6
Michael Robles <michaeljavier26@hotmail.com>
2022-01-13 22:35:51 -0500

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { NegociosComponent } from './negocios/negocios.component';
+import { LugaresComponent } from './lugares/lugares.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component';
+import { RouterModule, Route} from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { FooterComponent } from './footer/footer.component';
+
+const rutas:Route[] = [
+  {path: 'index', component:IndexComponent},
+  {path: 'acerca', component:AcercaComponent},
+  {path: 'historia', component:HistoriaComponent},
+  {path: 'lugares', component:LugaresComponent},
+  {path: 'negocios', component:NegociosComponent},
+  {path: 'ubicacion', component:UbicacionComponent},
+  {path: '**', redirectTo: 'index', pathMatch: 'full'}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AcercaComponent,
+    HistoriaComponent,
+    NegociosComponent,
+    LugaresComponent,
+    UbicacionComponent,
+    IndexComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
